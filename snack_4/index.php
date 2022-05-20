ccb7f4b12eddf6477e80f83beaee9dd58b1d63c4
MirkOrlando <mirko@orlando-m.it>
2022-05-20 16:08:31 +0200

    SNACK 4 create array of 15 different random numbers

---
+++ snack_4/index.php
@@ -0,0 +1,16 @@
+<?php
+/*
+## Snack 4
+Creare un array con 15 numeri casuali, 
+tenendo conto che l’array non dovrà 
+contenere lo stesso numero più di una volta
+*/
+
+$numbers = [];
+while (count($numbers) < 15) {
+    $number = rand(1, 100);
+    if (!in_array($number, $numbers)) {
+        $numbers[] = $number;
+    }
+}
+var_dump($numbers);
