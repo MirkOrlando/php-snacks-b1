cb0660379a3dda69365c49de3bbd85d8b106f2b6
MirkOrlando <mirko@orlando-m.it>
2022-05-23 12:03:46 +0200

    SNACK 3 add css container structure

--- snack_3/index.php
+++ snack_3/index.php
@@ -15,51 +15,47 @@ $posts = [
         [
             'title' => 'Post 1',
             'author' => 'Michele Papagni',
-            'text' => 'Testo post 1'
+            'text' => 'Testo post 1',
+            'img' => '<img src="https://picsum.photos/400/200" alt=""/>'
         ],
         [
             'title' => 'Post 2',
             'author' => 'Michele Papagni',
-            'text' => 'Testo post 2'
+            'text' => 'Testo post 2',
+            'img' => '<img src="https://picsum.photos/400/200" alt=""/>'
         ],
     ],
     '10/02/2019' => [
         [
             'title' => 'Post 3',
             'author' => 'Michele Papagni',
-            'text' => 'Testo post 3'
+            'text' => 'Testo post 3',
+            'img' => '<img src="https://picsum.photos/400/200" alt=""/>'
         ]
     ],
     '15/05/2019' => [
         [
             'title' => 'Post 4',
             'author' => 'Michele Papagni',
-            'text' => 'Testo post 4'
+            'text' => 'Testo post 4',
+            'img' => '<img src="https://picsum.photos/400/200" alt=""/>'
         ],
         [
             'title' => 'Post 5',
             'author' => 'Michele Papagni',
-            'text' => 'Testo post 5'
+            'text' => 'Testo post 5',
+            'img' => '<img src="https://picsum.photos/400/200" alt=""/>'
         ],
         [
             'title' => 'Post 6',
             'author' => 'Michele Papagni',
-            'text' => 'Testo post 6'
+            'text' => 'Testo post 6',
+            'img' => '<img src="https://picsum.photos/400/200" alt=""/>'
         ]
     ],
 ];
 
-/* 
-
-    <?php foreach ($posts as $post_date => $posts_content) : ?>
-        <p><?= $post_date ?></p>
-        <?php foreach ($posts_content as $post_content) : ?>
-            <?php foreach ($post_content as $key => $value) : ?>
-                <p><strong><?php $key ?>: </strong><?php $value ?></p>
-            <?php endforeach ?>
-        <?php endforeach ?>
-    <?php endforeach ?>
-    
+/*    
     foreach ($posts as $post_date => $posts_content) {
         echo '<p>' . $post_date . '</p>';
         foreach ($posts_content as $post_content) {
@@ -83,17 +79,36 @@ $posts = [
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Snack 3</title>
+    <style>
+        .container {
+            max-width: 1200px;
+            margin: 2rem auto;
+        }
+
+        .row {
+            display: flex;
+        }
+
+        .col-2 {
+            margin: 1rem;
+            width: calc(100% / 12 * 6 - 1rem);
+        }
+    </style>
 </head>
 
 <body>
-    <?php foreach ($posts as $post_date => $posts_content) : ?>
-        <p><?= $post_date ?></p>
-        <?php foreach ($posts_content as $post_content) : ?>
-            <?php foreach ($post_content as $key => $value) : ?>
-                <p><strong><?= $key ?>: </strong><?= $value ?></p>
-            <?php endforeach ?>
+    <div class="container">
+        <?php foreach ($posts as $post_date => $posts_content) : ?>
+            <div class="card">
+                <p><?= $post_date ?></p>
+                <?php foreach ($posts_content as $post_content) : ?>
+                    <?php foreach ($post_content as $key => $value) : ?>
+                        <p><strong><?= $key ?>: </strong><?= $value ?></p>
+                    <?php endforeach ?>
+                <?php endforeach ?>
+            </div>
         <?php endforeach ?>
-    <?php endforeach ?>
+    </div>
 
 </body>
 
