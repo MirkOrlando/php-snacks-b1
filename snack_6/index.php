34fba176e89849af216aa0becec442505c0383b0
MirkOrlando <mirko@orlando-m.it>
2022-05-23 12:39:45 +0200

    SNACK 6 print data

--- snack_6/index.php
+++ snack_6/index.php
@@ -5,27 +5,70 @@ Utilizzare questo array: https://pastebin.com/CkX3680A.
 Stampiamo il nostro array mettendo gli 
 insegnanti in un rettangolo grigio e i PM in un rettangolo verde.
 */
+
+$db = [
+    'teachers' => [
+        [
+            'name' => 'Michele',
+            'lastname' => 'Papagni'
+        ],
+        [
+            'name' => 'Fabio',
+            'lastname' => 'Forghieri'
+        ]
+    ],
+    'pm' => [
+        [
+            'name' => 'Roberto',
+            'lastname' => 'Marazzini'
+        ],
+        [
+            'name' => 'Federico',
+            'lastname' => 'Pellegrini'
+        ]
+    ]
+];
+
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <title>Title</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 6</title>
+    <style>
+        .container {
+            max-width: 400px;
+            margin: 2rem auto;
+            padding: 1rem;
+        }
 
-    <!-- Bootstrap CSS v5.1.3 -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        .border-gray {
+            border: 4px solid gray;
+        }
 
+        .border-green {
+            border: 4px solid green;
+        }
+    </style>
 </head>
 
 <body>
 
-    <!-- Bootstrap JavaScript Libraries -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <?php
+    foreach ($db as $key => $people) : ?>
+        <div class="container <?= $key == 'pm' ? 'border-green' : 'border-gray' ?>">
+            <?php
+            foreach ($people as $person) : ?>
+                <p><strong>Name: </strong><?= $person['name'] ?></p>
+                <p><strong>Lastname: </strong><?= $person['lastname'] ?></p>
+            <?php endforeach ?>
+        </div>
+    <?php endforeach ?>
+
 </body>
 
 </html>
