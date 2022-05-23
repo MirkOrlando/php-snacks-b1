18a623bde6aa3484803c91d113a0015a69a1a73a
MirkOrlando <mirko@orlando-m.it>
2022-05-23 12:25:30 +0200

    fix css

--- snack_5/index.php
+++ snack_5/index.php
@@ -7,7 +7,7 @@ Prendere il paragrafo e suddividerlo
 in tanti paragrafi. Ogni punto un nuovo paragrafo.
 */
 
-$text = 'Lorem ipsum. dolor sit amet consectetur adipisicing elit. Ea et molestiae expedita culpa fuga. nam aliquam eius cum. ipsum eos porro sunt hic perferendis illum. ipsa quaerat consequatur reprehenderit adipisci!';
+$text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis voluptatem impedit minima quidem nostrum! Dicta ducimus optio velit molestiae, eveniet non perspiciatis, ex eum eaque excepturi quae nesciunt ea esse magnam. Id commodi vero, quis ad ipsam similique illo repellat labore explicabo officia doloremque placeat exercitationem, atque voluptas quo. Neque minima maiores magnam incidunt cumque delectus perferendis, porro soluta, veritatis adipisci accusamus quas eligendi repudiandae excepturi. Praesentium excepturi accusantium consequuntur aliquam? Rem officia recusandae consequatur fugit sequi. Voluptatum voluptate a dignissimos, reiciendis ipsa amet dolore nesciunt, tempore iusto ipsum animi exercitationem iure velit natus! Consequuntur amet officiis quia deleniti necessitatibus rerum, veniam, eveniet modi beatae error nisi sunt reiciendis iusto iure itaque quo quos temporibus expedita minima, impedit ullam doloremque magnam ipsam numquam! Atque dolores magni consequuntur autem saepe molestiae eius perferendis, architecto nostrum iure ratione nam incidunt placeat! Temporibus quam numquam commodi, ipsam sapiente expedita architecto odit aperiam ducimus corrupti voluptatum id voluptas vitae saepe adipisci. Non nostrum, dolorum eos, ipsam dignissimos, porro nobis saepe laudantium distinctio numquam magni perspiciatis accusantium inventore rerum odit perferendis voluptate hic dolores sequi quis placeat molestiae. Incidunt iusto id reprehenderit magnam perspiciatis eum velit delectus corrupti quas cum ducimus, voluptas quo rerum expedita.';
 //var_dump(explode('.', $text));
 $paragraphs = explode('.', $text);
 
@@ -22,16 +22,23 @@ $paragraphs = explode('.', $text);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS v5.0.2 -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Bootstrap CSS v5.1.3 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <style>
+        p {
+            text-align: justify;
+        }
+    </style>
 
 </head>
 
 <body>
 
+    <h1>Paragraphs</h1>
     <?php
     foreach ($paragraphs as $p) : ?>
-        <p class="ms-2"><?= $p ?></p>
+        <p class="mx-2"><?= $p ?>.</p>
     <?php endforeach ?>
 
     <!-- Bootstrap JavaScript Libraries -->
